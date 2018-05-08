9b996b806396c05a1a4c0734054f7d7595487ccc
nagaminenot <nagamine@efg.company>
2018-05-08 22:08:22 +0900

    右下に常時コンテンツを表示することに成功

--- extention/project_b_ext/app/scripts/contentscript.ts
+++ extention/project_b_ext/app/scripts/contentscript.ts
@@ -2,3 +2,8 @@
 // import 'chromereload/devonly'
 
 console.log(`'Allo 'Allo! Content script`);
+
+const $extension = this.$extension = document.createElement('iframe');
+$extension.id = 'prj_b_sidebar';
+$extension.src = chrome.extension.getURL('pages/sidebar.html');
+document.body.appendChild($extension); 
