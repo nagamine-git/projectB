b6b92bc28a4b98d62bc948a6af34bc0e4c61058f
nagaminenot <nagamine@efg.company>
2018-05-20 15:37:06 +0900

    any対応

--- extention/project_b_ext/app/scripts/sidebar.ts
+++ extention/project_b_ext/app/scripts/sidebar.ts
@@ -1,6 +1,6 @@
 // 受信した、イベント一覧を展開してiframe内を置き換え
 window.addEventListener('message', function(event) {
-  let action_lists_dom = document.getElementById('action_lists');
+  let action_lists_dom: any = document.getElementById('action_lists');
   let action_lists_result = '';
   let action_number = 0;
   for (let action in event.data) {
@@ -18,19 +18,19 @@ window.addEventListener('message', function(event) {
 }, false);
 
 // クリック時の動作
-document.addEventListener('click', (e) => {
+document.addEventListener('click', (e: any) => {
   if (e.target.id == 'deleteAll') {
     // background.jsに送信
     chrome.runtime.sendMessage({'deleteAll':true},
     function(response){
       // 帰ってきたら、iframe内のsidebar.jsへ送信
-      let action_lists_dom = document.getElementById('action_lists');
+      let action_lists_dom: any = document.getElementById('action_lists');
       action_lists_dom.innerHTML = '';
     }); 
-  } else if (e.target.id.match(/action_delete_number_*/)) {
+  } else if (e.target.id.match(/actionaction_lists_dom_delete_number_*/)) {
     chrome.runtime.sendMessage({'delete':e.target.id.match(/\d+/)},
     function(response){
-      let action_lists_dom = document.getElementById('action_lists');
+      let action_lists_dom: any = document.getElementById('action_lists');
       let action_lists_result = '';
       let action_number = 0;
       for (let action in response) {
